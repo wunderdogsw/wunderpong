7c526764ddfb0e128e477115756fb92d1883ed69
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-09-18 15:07:19 +0300

    Updated visuals

---
+++ client/components/Doggos/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import React from 'react'
+
+import doggos from './pingpongdoggos.svg'
+
+export default () => (
+  <div className="Doggos">
+    <img src={doggos} alt="Ping pong doggos" />
+  </div>
+)
