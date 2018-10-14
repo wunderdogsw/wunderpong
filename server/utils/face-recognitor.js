148a7120ca501caec670e9460484f0d9d79529b9
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-10-14 12:17:58 +0300

    Cleanup

--- server/utils/face-recognitor.js
+++ server/utils/face-recognitor.js
@@ -11,68 +11,45 @@ const model = JSON.parse(fs.readFileSync('./server/utils/model.json'))
 const recognizer = fr.FaceRecognizer()
 recognizer.load(model)
 
+// size of the detected faces in pixels (rectangles)
 const size = 150
 
 
 // Predicts the players in a picture
 export const whoIsIt = imageBuffer => {
 
+  // Use opencv4nodejs to convert buffer image to correct format
+  // if dependencies are too heavey then fr.loadImage(path) can be used
+  // and opencv4nodejs dependency can be dropped
+  const imageCv = fr.CvImage(cv.imdecode(imageBuffer))
+  const image = fr.cvImageToImageRGB(imageCv)
 
-    // Use opencv4nodejs to convert buffer image to correct format
-    // if dependencies are too heavey then fr.loadImage(path) can be used
-    // and opencv4nodejs dependency can be dropped
-    const imageCv = fr.CvImage(cv.imdecode(imageBuffer))
-    const image = fr.cvImageToImageRGB(imageCv)
+  const rects = detector.locateFaces(image, size).map(mrect => mrect.rect)
+  if ( !rects.length ) return []
 
-    // size of the detected faces in pixels (rectangles)
-
-    // Assume 2 faces in image
-    const rects = detector.locateFaces(image, size).map(mrect => mrect.rect)
-    if ( !rects.length ) return []
-
-    const faces = detector.getFacesFromLocations(image, rects, size)
-    if ( !faces.length ) return []
-
-    // if (faces.length < 2) throw new Error('Could not find 2 faces in image')
-    // if (faces.length > 2) throw new Error('Found more than 2 faces in image')
-
-    // const players = faces.map(face => recognizer.predictBest(face))
+  const faces = detector.getFacesFromLocations(image, rects, size)
+  if ( !faces.length ) return []
 
 
+  const players = faces
 
-    const players = faces
-      .map(face => recognizer.predictBest(face))
-      .map((face, i) => ({
-        ...face,
-        ...rects[i],
-      }))
-      .sort((a, b) => b.right - a.right)
-      .map(player => ({
-        ...player,
-        name: player.className,
-      }))
+    // Get faces
+    .map(face => recognizer.predictBest(face))
 
+    // Map rect data to faces
+    .map((face, i) => ({
+      ...face,
+      ...rects[i],
+    }))
 
+    // Sort array so leftmost face is first
+    .sort((a, b) => b.right - a.right)
 
+    // Add name
+    .map(player => ({
+      ...player,
+      name: player.className,
+    }))
 
-    return players
-
-
-
-    // return rects[0].right > rects[0].right ? {
-    //   winner: players[0],
-    //   loser: players[0]
-    // } : {
-    //   winner: players[0],
-    //   loser: players[0]
-    // }
-
-    // Rightmost face is loser, left is winner
-    // return rects[0].right > rects[1].right ? {
-    //   winner: players[0],
-    //   loser: players[1]
-    // } : {
-    //   winner: players[1],
-    //   loser: players[0]
-    // }
+  return players
 }
