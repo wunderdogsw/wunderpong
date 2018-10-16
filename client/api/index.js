70922601f2020c17114ad7f1bc18af43a5a45411
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-10-16 14:14:48 +0300

    luffis.local to localhost

--- client/api/index.js
+++ client/api/index.js
@@ -7,7 +7,7 @@ import {
 
 const API_URL = process.env.NODE_ENV === 'production' ?
   process.env.PRODUCTION_API_URL :
-  'http://luffis.local:3000/api'
+  'http://localhost:3000/api'
 
 
 export const getLadder = () => fetchJson(`${API_URL}/ladder`)
