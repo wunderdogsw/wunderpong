46be5e0115cf9c3af1f7544874f3a3d6f5e430ce
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2018-11-08 16:15:26 +0200

    Add Dockerfile

--- client/api/index.js
+++ client/api/index.js
@@ -6,7 +6,7 @@ import {
 
 
 const API_URL = process.env.NODE_ENV === 'production' ?
-  process.env.PRODUCTION_API_URL :
+  (process.env.PRODUCTION_API_URL || '/api') :
   'http://localhost:3000/api'
 
 
