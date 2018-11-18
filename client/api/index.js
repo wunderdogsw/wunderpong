9d8746a3f57686fb89acd0d9727030bf14fff02b
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-11-18 17:40:58 +0200

    Made client dev env a bit more flexible

--- client/api/index.js
+++ client/api/index.js
@@ -7,7 +7,7 @@ import {
 
 const API_URL = process.env.NODE_ENV === 'production' ?
   (process.env.PRODUCTION_API_URL || '/api') :
-  'http://localhost:3000/api'
+  (process.env.DEVELOPMENT_API_URL || 'http://localhost:3000/api')
 
 
 export const getLadder = () => fetchJson(`${API_URL}/ladder`)
