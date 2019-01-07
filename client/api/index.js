01b4253c622c4cf1a9a4f61983bcc94a517a21ef
Viljami Anttonen <viljami.anttonen@gmail.com>
2019-01-07 14:18:47 +0200

    Enabled facerecognition

--- client/api/index.js
+++ client/api/index.js
@@ -9,6 +9,10 @@ const API_URL = process.env.NODE_ENV === 'production' ?
   (process.env.PRODUCTION_API_URL || '/api') :
   (process.env.DEVELOPMENT_API_URL || 'http://localhost:3000/api')
 
+const FACE_RECOGNITION_URL =
+  process.env.FACE_RECOGNITION_URL ||
+  'https://face-recognitor.lab.wunder.dog'
+
 
 export const getLadder = () => fetchJson(`${API_URL}/ladder`)
 export const getPlayers = () => fetchJson(`${API_URL}/players`)
@@ -16,7 +20,7 @@ export const postMatch = (winner, loser) => fetchJson(`${API_URL}/match`, {
   method: 'POST',
   body: JSON.stringify({ text: `${sanitizeName(winner)} ${sanitizeName(loser)}`})
 })
-export const postScreenshot = image => fetchJson(`${API_URL}/whoisit`, {
+export const postScreenshot = image => fetchJson(`${FACE_RECOGNITION_URL}/whoisit`, {
   method: 'POST',
   body: JSON.stringify({ image })
 })
